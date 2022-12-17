75815d485930c9a58e36dac8d65dfbb86a9948dd
pindariyanita12 <nita.p@simformsolutions.com>
2022-12-17 12:32:34 +0530

    rmoved style from livecases.php

--- livecases.php
+++ livecases.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php include 'link/links.php'; ?>
-    <?php include 'css/style.php'; ?>
+    <?php include 'css/.php'; ?>
 
 </head>
 
